3aed8245d65d7166a3562e3c5f7cc3c39827605d
matheusrpd <matheusrpd@gmail.com>
2021-06-23 16:48:01 -0300

    Adicionado componente de codigo da sala.

---
+++ src/components/RoomCode/index.tsx
@@ -0,0 +1,25 @@
+import copyImg from '../../assets/copy.svg';
+
+import './styles.scss';
+
+type RoomCodeProps = {
+  code: string;
+}
+export function RoomCode({ code }: RoomCodeProps) {
+
+  function copyRoomCodeToClipboard() {
+    navigator.clipboard.writeText(code);
+  }
+
+  return (
+    <button
+      className="room-code"
+      onClick={copyRoomCodeToClipboard}
+    >
+      <div>
+        <img src={copyImg} alt="Copy room code" />
+      </div>
+      <span>Sala #{code}</span>
+    </button>
+  );
+}
--- src/pages/Room/index.tsx
+++ src/pages/Room/index.tsx
@@ -1,15 +1,23 @@
+import { useParams } from 'react-router-dom';
 import logoImg from '../../assets/logo.svg';
 import { Button } from '../../components/Button';
+import { RoomCode } from '../../components/RoomCode';
 
 import './styles.scss';
 
+type RoomParams = {
+  id: string;
+}
+
 export function Room() {
+  const { id } = useParams<RoomParams>();
+
   return (
     <div id="page-room">
       <header>
         <div className="content">
           <img src={logoImg} alt="Letmeask" />
-          <div>código</div>
+          <RoomCode code={id} />
         </div>
       </header>
 
